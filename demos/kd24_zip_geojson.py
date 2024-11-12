5de3e0fd3a063245f3a0190929940716044af783
Daníel Örn Árnason <danielarnason85@gmail.com>
2024-11-12 14:03:09 +0100

    Simplification of demo

--- demos/kd24_zip_geojson.py
+++ demos/kd24_zip_geojson.py
@@ -3,25 +3,23 @@ from core import *
 from engine import *
 
 # Imports fra Pyhton standard
-import requests as r
 from pathlib import Path
 import zipfile
+
+# Definer reader, worker og output
+reader = Input_Reader
+worker = Worker
+output = Output_Writer
 
 # Download zip fil fra URL
 zip_file_url = 'URL TIL ZIP FIL MED GEOJSON FILER'
 local_file_path = Path('c:/temp/geojson_data.zip')
 extract_path = Path('c:/temp/geojson_data')
 
-try:
-    with r.get(zip_file_url, stream=True) as response:
-        response.raise_for_status()
-        with open(local_file_path, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
-    print(f'Download complete: {local_file_path}')
-except r.exceptions.RequestException as e:
-    print(f'An error occurred: {e}')
+# Download fil
+download_successful = worker.download_file(zip_file_url, local_file_path)
 
+# Unzip fil
 try:
     with zipfile.ZipFile(local_file_path, 'r') as zip_ref:
         zip_ref.extractall(extract_path)
@@ -30,10 +28,6 @@ except zipfile.BadZipFile:
     print(f'Error: {local_file_path} is not at zip file or it is corrupted')
 
 # Bearbejd data i Q-ETL og gem i database
-reader = Input_Reader
-worker = Worker
-output = Output_Writer
-
 # Find alle geojson filer fra zip fil
 geoJson_filer = [file for file in Path(extract_path).rglob('*.geojson')]
 
