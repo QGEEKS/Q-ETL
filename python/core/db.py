5e6137b60a428644aa6b26b7a73adfa7d5f44009
MFuglsang <morten.w.fuglsang@gmail.com>
2024-11-24 09:34:28 +0100

    Error handeling
    We dont wont jobs to die on behalf of the internal db...

--- python/core/db.py
+++ python/core/db.py
@@ -19,49 +19,61 @@ def initdb():
         logger.info(f'Unable to use internal DB')
     
 def populatedb(conn):
-    sql_inittable = """
-        CREATE TABLE IF NOT EXISTS jobs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            run_id INTEGER NOT NULL,
-            jobname text NOT NULL, 
-            start_time TEXT, 
-            end_time TEXT,
-            status TEXT,
-            logfile TEXT 
-        );"""
-    
-    cursor = conn.cursor()
-    cursor.execute(sql_inittable) 
-    conn.commit()
+    try:
+        sql_inittable = """
+            CREATE TABLE IF NOT EXISTS jobs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                run_id INTEGER NOT NULL,
+                jobname text NOT NULL, 
+                start_time TEXT, 
+                end_time TEXT,
+                status TEXT,
+                logfile TEXT 
+            );"""
+        
+        cursor = conn.cursor()
+        cursor.execute(sql_inittable) 
+        conn.commit()
+    except:
+        pass
     
 def is_db_populated (conn):
-    sql_check = """SELECT count(*) FROM sqlite_master WHERE type='table' AND name='jobs';""" 
-    cursor = conn.cursor()
-    cursor.execute(sql_check) 
-    rows = cursor.fetchall()  
-    if rows[0][0] == 1:
-        return True
-    else:
+    try:
+        sql_check = """SELECT count(*) FROM sqlite_master WHERE type='table' AND name='jobs';""" 
+        cursor = conn.cursor()
+        cursor.execute(sql_check) 
+        rows = cursor.fetchall()  
+        if rows[0][0] == 1:
+            return True
+        else:
+            return False
+    except:
         return False
 
 def startjob(jobrun, name, time, logfile):
-    db_dir = os.getcwd() + '/db'
-    db_file = db_dir + '/internal.db'
-    conn = sqlite3.connect(db_file) 
-    cursor = conn.cursor()
-    sql_insert =  f"""INSERT INTO jobs (run_id, jobname, start_time, end_time, status, logfile) VALUES ('{jobrun}', '{name}', '{time}', NULL, 'Running', '{logfile}')"""
-    cursor.execute(sql_insert)
-    conn.commit()
-    conn.close() 
+    try:
+        db_dir = os.getcwd() + '/db'
+        db_file = db_dir + '/internal.db'
+        conn = sqlite3.connect(db_file) 
+        cursor = conn.cursor()
+        sql_insert =  f"""INSERT INTO jobs (run_id, jobname, start_time, end_time, status, logfile) VALUES ('{jobrun}', '{name}', '{time}', NULL, 'Running', '{logfile}')"""
+        cursor.execute(sql_insert)
+        conn.commit()
+        conn.close() 
+    except:
+        pass
 
 def update_job(jobrun, status, time):
-    db_dir = os.getcwd() + '/db'
-    db_file = db_dir + '/internal.db'
-    conn = sqlite3.connect(db_file) 
-    cursor = conn.cursor()
-    sql_insert =  f"""UPDATE jobs SET status =  '{status}', end_time = '{time}' WHERE run_id = '{jobrun}'"""
-    cursor.execute(sql_insert)
-    conn.commit()
-    conn.close() 
+    try:
+        db_dir = os.getcwd() + '/db'
+        db_file = db_dir + '/internal.db'
+        conn = sqlite3.connect(db_file) 
+        cursor = conn.cursor()
+        sql_insert =  f"""UPDATE jobs SET status =  '{status}', end_time = '{time}' WHERE run_id = '{jobrun}'"""
+        cursor.execute(sql_insert)
+        conn.commit()
+        conn.close()
+    except:
+        pass
 
     
