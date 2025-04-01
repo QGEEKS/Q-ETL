47a6e7353df48ec330e492b34f35620b89054788
Morten Fuglsang <morten.w.fuglsang@gmail.com>
2025-04-01 09:44:04 +0200

    Update generic_processing.py

--- examples/generic_processing.py
+++ examples/generic_processing.py
@@ -11,7 +11,7 @@ input_reader = Input_Reader
 wfslayer = input_reader.wfs('https://geofyn.admin.gc2.io/wfs/geofyn/fynbus/25832?SERVICE=WFS&REQUEST=GetFeature&VERSION=1.1.0&TYPENAME=fynbus:routes_25832_v&SRSNAME=urn:ogc:def:crs:EPSG::25832')
 
 params = {
-    'INPUT':"WFS:// pagingEnabled='true' preferCoordinatesForWfsT11='false' srsname='EPSG:25832' typename='fynbus:stops_v' url='https://geofyn.admin.gc2.io/wfs/geofyn/fynbus/25832' version='auto'",
+    'INPUT': wfslayer,
     'DISTANCE':10,
     'SEGMENTS':5,
     'END_CAP_STYLE':0,
